22912526520cbf18e52164fe058ea88cf0eb3e95
Gage Rowden <gage.rowden1145@gmail.com>
2024-09-18 11:50:25 -0500

    Removed pre-initialized colors and just use the RGB values within the gameLoop function.

--- tangle_wrangle.py
+++ tangle_wrangle.py
@@ -7,14 +7,6 @@ def run_tangle_wrangle():
     # Initialize pygame
     pygame.init()
 
-    # Define colors
-    white = (255, 255, 255)
-    yellow = (255, 255, 102)
-    black = (0, 0, 0)
-    red = (213, 50, 80)
-    green = (0, 255, 0)
-    blue = (50, 153, 213)
-
     # Display settings
     width = 600
     height = 400
